9e2c1e11dd7ad5745029a49211b9a18927cbcd83
Silvan Engeler <github@threema.ch>
2015-08-25 08:18:58 +0200

    sdk tool only executable/includable in a cli

--- threema-msgapi-tool.php
+++ threema-msgapi-tool.php
@@ -5,12 +5,19 @@
  * @copyright Copyright (c) 2015, Threema GmbH
  */
 
+//disallow using the cli tool in a web project
+if ('cli' !== php_sapi_name()
+	|| null === $argv) {
+	//file not called from the cli
+	die('please run '.basename(__FILE__).' only in a cli. To use the threema msgapi sdk in your web project, include the source/bootstrap.php or the threema_msgapi.phar file.');
+}
 try {
 include 'threema_msgapi.phar';
 
 	//require a valid PublicKeyStore; create an empty file if necessary
-	touch('keystore.txt');
-	$fileKeyStore = new Threema\MsgApi\PublicKeyStores\File('keystore.txt');
+	$pathToKeyStoreFile = 'keystore.txt';
+	touch($pathToKeyStoreFile);
+	$fileKeyStore = new Threema\MsgApi\PublicKeyStores\File($pathToKeyStoreFile);
 
 	$tool = new \Threema\Console\Run($argv, $fileKeyStore);
 	$tool->run();

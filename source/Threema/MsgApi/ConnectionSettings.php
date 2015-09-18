6a4faac36b14ca98d3083198c5646fb1323994ca
rugk <rugk@users.noreply.github.com>
2015-09-18 21:05:47 +0200

    add $tlsOptions
    also executed php-cs-fixer, so that's the reason why the complete files
    are displayed as changed

--- source/Threema/MsgApi/ConnectionSettings.php
+++ source/Threema/MsgApi/ConnectionSettings.php
@@ -7,52 +7,97 @@
 
 namespace Threema\MsgApi;
 
-class ConnectionSettings {
-	/**
-	 * @var string
-	 */
-	private $threemaId;
+class ConnectionSettings
+{
+    /**
+     * @var string
+     */
+    private $threemaId;
 
-	/**
-	 * @var string
-	 */
-	private $secret;
+    /**
+     * @var string
+     */
+    private $secret;
 
-	/**
-	 * @var string
-	 */
-	private $host;
+    /**
+     * @var string
+     */
+    private $host;
 
-	/**
-	 * @param string $threemaId valid threema id (8chars)
-	 * @param string $secret secret
-	 * @param string $host server url
-	 */
-	function __construct($threemaId, $secret, $host = null) {
-		$this->threemaId = $threemaId;
-		$this->secret = $secret;
-		if ($host === null) $host = 'https://msgapi.threema.ch';
-		$this->host = $host;
-	}
+    /**
+     * @var array
+     */
+    private $tlsOptions;
 
-	/**
-	 * @return string
-	 */
-	public function getThreemaId() {
-		return $this->threemaId;
-	}
+    /**
+     * @param string $threemaId valid threema id (8chars)
+     * @param string $secret secret
+     * @param string $host server url
+     */
+    public function __construct($threemaId, $secret, $host = null, $tlsOptions = null)
+    {
+        $this->threemaId = $threemaId;
+        $this->secret = $secret;
+        if ($host === null) {
+            $host = 'https://msgapi.threema.ch';
+        }
+        $this->host = $host;
 
-	/**
-	 * @return string
-	 */
-	public function getSecret() {
-		return $this->secret;
-	}
+        // TLS options
+        if ($tlsOptions !== null && is_array($tlsOptions) && !empty($tlsOptions)) {
+            if (!array_key_exists('forceHttps', $tlsOptions)) {
+                $tlsOptions['forceHttps'] = false;
+            }
+            if (!array_key_exists('tslVersion', $tlsOptions)) {
+                $tlsOptions['tslVersion'] = null;
+            }
+            if (!array_key_exists('tslCipher', $tlsOptions)) {
+                $tlsOptions['tslCipher'] = null;
+            }
 
-	/**
-	 * @return string
-	 */
-	public function getHost() {
-		return $this->host;
-	}
+            $this->tlsOptions = $tlsOptions;
+        } else {
+            $this->tlsOptions = null;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getThreemaId()
+    {
+        return $this->threemaId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSecret()
+    {
+        return $this->secret;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTlsOptions()
+    {
+        return $this->tlsOptions;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTlsOption($option)
+    {
+        return $this->tlsOptions[$option];
+    }
 }

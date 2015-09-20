7ffa5bf50263edc18a8920d4bd8ba16d842ab4c9
rugk <rugk@users.noreply.github.com>
2015-09-20 18:43:11 +0200

    [PhpKeyStore] Some small fixes
    corrected include to use phar, run php-cs-fixer over changed
    files/changed tabs to spaces

--- threema-msgapi-tool.php
+++ threema-msgapi-tool.php
@@ -7,22 +7,21 @@
 
 //disallow using the cli tool in a web project
 if ('cli' !== php_sapi_name()
-	|| null === $argv) {
-	//file not called from the cli
-	die('please run '.basename(__FILE__).' only in a cli. To use the threema msgapi sdk in your web project, include the source/bootstrap.php or the threema_msgapi.phar file.');
+    || null === $argv) {
+    //file not called from the cli
+    die('please run '.basename(__FILE__).' only in a cli. To use the threema msgapi sdk in your web project, include the source/bootstrap.php or the threema_msgapi.phar file.');
 }
 try {
-	include '..\source\bootstrap.php';
+    include 'threema_msgapi.phar';
 
-	//require a valid PublicKeyStore; create an empty file if necessary
-	$pathToKeyStoreFile = 'keystore.php';
-	touch($pathToKeyStoreFile);
-	$publicKeyStore = new Threema\MsgApi\PublicKeyStores\PhpFile($pathToKeyStoreFile);
+    //require a valid PublicKeyStore; create an empty file if necessary
+    $pathToKeyStoreFile = 'keystore.php';
+    touch($pathToKeyStoreFile);
+    $publicKeyStore = new Threema\MsgApi\PublicKeyStores\PhpFile($pathToKeyStoreFile);
 
-	$tool = new \Threema\Console\Run($argv, $fileKeyStore);
-	$tool->run();
-}
-catch (\Threema\Core\Exception $exception) {
-	echo "ERROR: ".$exception->getMessage()."\n";
-	die();
+    $tool = new \Threema\Console\Run($argv, $fileKeyStore);
+    $tool->run();
+} catch (\Threema\Core\Exception $exception) {
+    echo "ERROR: ".$exception->getMessage()."\n";
+    die();
 }
